32c7e2ca77f26a17b055bc3dc7383787cfd76d93
qb1968 <qb1968@aol.com>
2021-02-28 13:03:13 -0500

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import About from './components/About';
+import Contact from './components/Contact';
+import Home from './components/Home';
+import Projects from './components/Projects';
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+      <About/>
+      <Projects/>
+      <Contact/>
+    </div>
+  );
+}
+
+export default App;
