efcd25672ae2b30a565caa61d7fdf2865eb542d0
qb1968 <qb1968@aol.com>
2021-09-26 11:48:57 -0400

    RaceCity

--- src/effects/data.js
+++ src/effects/data.js
@@ -1,77 +1,69 @@
 import RM from '../images/rick.png'
-import ECOM from '../images/ecom.png'
+import ECOM from '../images/RaceCityComics.png'
 import SFMTA from '../images/SFMTA.png'
 import Movie from '../images/Movies.png'
 import Super from '../images/superhero.jpg'
 import Memory from '../images/memories.jpg'
 
-const info = 
-     [ 
-    
-    {
-        id: 0,
-        name: 'SFMTA DATA ANALYSIS',
-        tools: 'React',
-        about: 'Collaborated with Data Science, UX, and Web. Member of the FrontEnd team. Site was created to moniter bunches and gaps for the SFMTA',
-        link: 'https://labs2020.vercel.app/',
-        github: 'https://github.com/qb1968/Labs2020',
-        image: SFMTA
-
-    },
+const info = [
+  {
+    id: 0,
+    name: "SFMTA DATA ANALYSIS",
+    tools: "React",
+    about:
+      "Collaborated with Data Science, UX, and Web. Member of the FrontEnd team. Site was created to moniter bunches and gaps for the SFMTA",
+    link: "https://labs2020.vercel.app/",
+    github: "https://github.com/qb1968/Labs2020",
+    image: SFMTA,
+  },
     {
         id: 1,
-        name: 'Ecom',
-        tools: 'React',
-        about: 'Fully functional ecommerce site with paypal capability.',
-        link: 'https://morning-gorge-25166.herokuapp.com/',
-        github: 'https://github.com/qb1968/ecom',
-        image: ECOM,
-        
+        name: "RaceCityComics",
+        tools: "React, Express",
+        about: "Fully functional ecommerce site with paypal capability.",
+        link: "https://www.racecitycomics.com",
+        github: "https://github.com/qb1968/racecity2.0",
+        image:  ECOM
+      ,
+  },
+  {
+    id: 2,
+    name: "Memories",
+    tools: "React, Redux, Material-UI",
+    link: "https://suspicious-carson-ecf6e2.netlify.app/",
+    about: "Project to post some of your favorite memories.",
+    github: "https://github.com/qb1968/project_mern_memories-front-end-",
+    image: Memory,
+  },
+  {
+    id: 3,
+    name: "Rick and Morty",
+    tools: "React",
+    about: "Learn about your favorite character",
+    link: "https://rick-morty.markallison751.now.sh/",
+    github: "https://github.com/qb1968/Rick-Morty",
+    image: RM,
+  },
 
-    },
-    {
-        id: 2,
-        name: 'Memories',
-        tools: 'React, Redux, Material-UI',
-        link: 'https://suspicious-carson-ecf6e2.netlify.app/',
-        about: 'Project to post some of your favorite memories.',
-        github: 'https://github.com/qb1968/project_mern_memories-front-end-',
-        image: Memory
-
-    },
-    {
-        id: 3,
-        name: 'Rick and Morty',
-        tools: 'React',
-        about: 'Learn about your favorite character',
-        link: 'https://rick-morty.markallison751.now.sh/',
-        github: 'https://github.com/qb1968/Rick-Morty',
-        image: RM
-
-    },
-    
-    
-   
-    {
-        id: 4,
-        name: 'Movie-DB',
-        tools: 'React',
-        about:'Learn about your favorite movies past and present.',
-        link: 'https://movie-db-five.now.sh/',
-        github: 'https://github.com/qb1968/MovieDB',
-        image: Movie
-
-    },
-    {
-        id: 5,
-        name: 'Superhero-Finder',
-        tools: 'React',
-        about: 'Still just a big kid at heart and also a giant nerd. Type in your favorite hero and see what pops up.',
-        link: 'https://superhero-finder.vercel.app/',
-        github: 'https://github.com/qb1968/Superhero-Finder',
-        image: Super
-
-    },
-]
+  {
+    id: 4,
+    name: "Movie-DB",
+    tools: "React",
+    about: "Learn about your favorite movies past and present.",
+    link: "https://movie-db-five.now.sh/",
+    github: "https://github.com/qb1968/MovieDB",
+    image: Movie,
+  },
+  {
+    id: 5,
+    name: "Superhero-Finder",
+    tools: "React",
+    about:
+      "Still just a big kid at heart and also a giant nerd. Type in your favorite hero and see what pops up.",
+    link: "https://superhero-finder.vercel.app/",
+    github: "https://github.com/qb1968/Superhero-Finder",
+    image: Super,
+  },
+];
  
 export default info
